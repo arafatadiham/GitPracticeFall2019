3bd83cbb29054283dc9010ccc0350d573c863981
arafatadiham <arafat.adiham@gmail.com>
2020-02-29 11:32:19 -0800

    my first commit
    Signed-off-by: arafatadiham <arafat.adiham@gmail.com>

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -1,4 +1,13 @@
 package day1;
 
-public class StringPractice {
+public class StringPractice{
+    public static void main(String[]args){
+
+        System.out.println("The first day of Git");
+
+        System.out.println("first commit is successfull now we are doing second commit !!!!");
+
+
+    }
+
 }

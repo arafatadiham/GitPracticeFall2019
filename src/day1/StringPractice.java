0f30f4695b4d726a2c1d9a75030332d0a46b868e
arafatadiham <arafat.adiham@gmail.com>
2020-02-29 12:06:12 -0800

    my second commit

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -1,7 +1,7 @@
 package day1;
 
-public class StringPractice{
-    public static void main(String[]args){
+public class StringPractice {
+    public static void main(String[] args) {
 
         System.out.println("The first day of Git");
 
